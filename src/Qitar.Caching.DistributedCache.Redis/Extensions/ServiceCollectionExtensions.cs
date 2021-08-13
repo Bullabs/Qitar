d7bb36bd345c560dc96d35bbc37d5f105842b862
Hobbs1210 <habibzaid@gmail.com>
2021-08-13 15:46:29 -0500

    Redis support

---
+++ src/Qitar.Caching.DistributedCache.Redis/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Qitar.Caching.DistributedCache.Extensions;
+using System;
+
+namespace Qitar.Caching.DistributedCache.Redis.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDistributedMemoryCache(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddDistributedCache();
+            services.AddStackExchangeRedisCache(options => { });
+
+            return services;
+        }
+    }
+}

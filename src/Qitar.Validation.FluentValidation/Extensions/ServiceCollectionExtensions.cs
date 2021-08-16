0149fdae8f7055fa069c747b83500219d2ef8c5c
Hobbs1210 <habibzaid@gmail.com>
2021-08-16 14:39:54 -0500

    validation

---
+++ src/Qitar.Validation.FluentValidation/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Qitar.Validation.FluentValidation.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDapperRepository(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddScoped(typeof(IRepository<>), typeof(DapperRepository<>));
+
+            return services;
+        }
+    }
+}
---
+++ src/Qitar.Validation.FluentValidation/FluentValidationProvider.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Qitar.Commands;
+using Qitar.Dependencies;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Qitar.Validation.FluentValidation
+{
+    public class FluentValidationProvider : IValidationProvider
+    {
+        private readonly IResolveHandler _resolveHandler;
+
+        public FluentValidationProvider(IResolveHandler resolveHandler)
+        {
+            _resolveHandler = resolveHandler;
+        }
+
+        public async  ValueTask<IValidationResponse> Validate<TCommand>(TCommand command, CancellationToken canclationToken = default) where TCommand : ICommand
+        {
+            var validator = _resolveHandler.ResolveHandler<IValidator<TCommand>>();
+
+            if(validator is null)
+            {
+                throw new ValidationException($"{nameof(command)} Validator is not found");
+            }
+
+            var validationResult = await validator.ValidateAsync(command).ConfigureAwait(false);
+
+            var errors = string.Concat(validationResult.Errors.Select(s => $"{s.ErrorCode}-{s.ErrorMessage};"));
+            return new ValidationResponse(errors);
+        }
+    }
+}

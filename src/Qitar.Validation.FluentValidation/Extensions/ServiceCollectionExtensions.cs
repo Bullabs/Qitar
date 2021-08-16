2b2ca4e0e345e989701092119487760872f793c5
Habib Zaid <habibzaid@gmail.com>
2021-08-16 14:44:15 -0500

    Validation (#6)
    * validation
    
    * validation

---
+++ src/Qitar.Validation.FluentValidation/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Qitar.Validation.FluentValidation.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDapperRepository(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddTransient<IValidationProvider, FluentValidationProvider>();
+
+            return services;
+        }
+    }
+}

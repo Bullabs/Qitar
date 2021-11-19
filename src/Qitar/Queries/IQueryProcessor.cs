aa38f76a337daaa25e14f2439d4efabead17cc28
Habib Zaid <habibzaid@gmail.com>
2021-11-19 12:51:08 -0600

    query

--- src/Qitar/Queries/IQueryProcessor.cs
+++ src/Qitar/Queries/IQueryProcessor.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading;
+﻿using Qitar.Objects.Results;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Qitar.Queries
 {
     public interface IQueryProcessor
     {
-        ValueTask<TResult> Process<TQuery, TResult>(TQuery query, CancellationToken cancellationToken = default) where TQuery : IQuery<TResult>;
+        ValueTask<TResult> Process<TQuery, TResult>(TQuery query, CancellationToken cancellationToken = default) where TQuery : IQuery<TResult> where TResult : IResult;
     }
 }

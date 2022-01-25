874e3a9faea07f9de41f5db7d3cfeafa3cd5a59c
Habib Zaid <habibzaid@gmail.com>
2022-01-25 15:34:38 -0600

    Update ICommandHandler.cs

--- src/Qitar/Commands/ICommandHandler.cs
+++ src/Qitar/Commands/ICommandHandler.cs
@@ -1,8 +1,12 @@
 using Qitar.Messages;
+using Qitar.Objects.Results;
 
 namespace Qitar.Commands
 {
     public interface ICommandHandler<in TCommand> : IMessageHandler<TCommand, ICommandResult> where TCommand : ICommand
+    {
+    }
+    public interface ICommandHandler<in TCommand, TCommandResult> : IMessageHandler<TCommand, TCommandResult> where TCommand : ICommand where TCommandResult : IResult
     {
     }
 }

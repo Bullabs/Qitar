727e925988422d534c7b22f18e8670ea4cba8f52
Hobbs1210 <habibzaid@gmail.com>
2022-03-18 07:41:05 -0500

    oracle database support

---
+++ src/Qitar.Store.Oracle/OracleDatabaseConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using Qitar.Store.Connections;
+using Qitar.Utils;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Qitar.Store.Oracle
+{
+    public class OracleDatabaseConnectionFactory : ISqlConnectionFactory
+    {
+        public async ValueTask<IDbConnection> OpenConnection(string connectionString, CancellationToken cancellationToken)
+        {
+            connectionString.NotNull();
+
+            var connection = new OracleConnection(connectionString);
+            await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+            return connection;
+        }
+    }
+}
